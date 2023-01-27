33d1770797164e2c2f733362ba340886bd145a3c
Aidana777 <aidananiazalieva27@gmail.com>
2023-01-27 14:16:27 +0600

    nhnfnb

--- main.js
+++ main.js
@@ -1,63 +1,40 @@
-class Component {
+const color = prompt('Выьерите цвет')
+
+class Svetofor {
     constructor(selector) {
         this.$element = document.querySelector(selector)
     }
-
-    hide() {
-        vvc
-        this.$element.style.display = 'none'
-    }
-
-    show() {
-        this.$element.style.display = 'block'
-    }
 }
 
-class Block extends Component {
+class Circle extends Svetofor {
     constructor(options) {
         super(options.selector);
-
-        this.$element.style.width = this.$element.style.height = options.razmer + 'px'
-        this.$element.style.background = options.cvet
+        this.$element.style.backgroundColor = options.color
         this.$element.style.borderRadius = '50%'
-        let color = prompt('')
-        if (color === "red") {
-            red.red()
-        }else if(color==='yellow'){
-            yellow.yellow()
-        }
+        his.$element.style.width = this.$element.style.height = options.razmer + 'px'
+        this.$element.style.borderRadius = '50%'
     }
 }
 
-const block1 = new Block({
-    selector: '#block1',
-    razmer: 90,
-    cvet: 'red'
-})
+if (color === 'красный') {
+    const red = new Circle({
+        selector: '#red',
+        razmer: 90,
+        color: 'red'
 
-const block2 = new Block({
-    selector: '#block2',
-    razmer: 90,
-    cvet: 'yellow'
-})
-
-class Circle extends Block {
-    constructor(options) {
-        super(options);
-        this.$element.style.borderRadius = '50%'
-       
-    }
+    })
+} else if (color === 'желтый') {
+    const yellow = new Circle({
+        selector: '#yellow',
+        razmer: 90,
+        color: 'yellow'
+    })
+} else if (color === 'зеленый') {
+    const green = new Circle({
+        selector: '#green',
+        razmer: 90,
+        color: 'green'
+    })
+} else {
+    alert('ERROR')
 }
-
-const circle = new Circle({
-    selector: '.krug',
-    razmer: 90,
-    cvet: 'green'
-})
-
-circle.hide()
-circle.show()
-block1.hide()
-block1.show()
-block2.hide()
-block2.show()
